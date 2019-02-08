424a10619be97f48facad4ee508ad0692b709a68
Administrator <Administrator@smart20101>
2019-02-08 10:38:18 +0900

    [오상택]sampleCRUD 프로젝트 초기화

---
+++ web04/src/main/java/com/bonobono/web04/Web04Application.java
@@ -0,0 +1,14 @@
+package com.bonobono.web04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Web04Application.class, args);
+	}
+
+}
+
